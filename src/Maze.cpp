09ad9de96fa0e74e0e4384d0483e9dfddcd83d91
SGREG <barani@>
2026-04-06 19:51:40 +0300

    Init commit

---
+++ src/Maze.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+class Maze
+{
+public:
+
+    std::vector<std::vector<bool>> tiles; 
+
+    Maze(int newSizeX, int newSizeY)
+    {
+        sizeX = newSizeX;
+        sizeY = newSizeY;
+    }
+
+    void setSizeX(int newSizeX)
+    {
+        sizeX = newSizeX;
+    }
+    void setSizeY(int newSizeY)
+    {
+        sizeY = newSizeY;
+    }
+
+    int getSizeX()
+    {
+        return sizeX;
+    }
+    int getSizeY()
+    {
+        return sizeY;
+    }
+
+private:
+
+    int sizeX;
+    int sizeY;
+
+};
