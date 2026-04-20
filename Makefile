e9c89a1a9314ba42d640582177615e4adaf026c8
SGREG <barani@>
2026-04-20 19:47:39 +0300

    feat: maze and drawer classes

---
+++ Makefile
@@ -0,0 +1,3 @@
+all: main.cpp
+	g++ ./Maze.cpp -o ./maize -lsfml-graphics -lsfml-window -lsfml-system
+
--- src/Maze.cpp
+++ src/Maze.cpp
@@ -1,38 +1,132 @@
 #include <vector>
+#include <cstdlib>
+#include <cstdint>
+#include <concepts>
+#include <unordered_map>
+#include <SFML/Graphics.hpp>
+
+struct Tile 
+{
+
+    bool isWall; // Is tile a wall
+    uint32_t status; 
+
+};
+
 
 class Maze
 {
 public:
 
-    std::vector<std::vector<bool>> tiles; // Maze tiles 
+    std::vector<std::vector<Tile>> tiles; // Maze tiles 
 
-    Maze(int newSizeX, int newSizeY)
+    Maze(int newrows = 1, int newcoloumns = 1)
     {
-        sizeX = newSizeX;
-        sizeY = newSizeY;
-    }
-
-    void setSizeX(int newSizeX)
-    {
-        sizeX = newSizeX;
-    }
-    void setSizeY(int newSizeY)
-    {
-        sizeY = newSizeY;
+        rows = newrows;
+        coloumns = newcoloumns;
+        tiles.resize(rows);
+        for(auto& rows : tiles)
+        {
+            rows.resize(coloumns);
+        }
     }
 
-    int getSizeX()
+    void setRows(int newrows)
     {
-        return sizeX;
+        rows = newrows;
     }
-    int getSizeY()
+    void setColoumns(int newcoloumns)
     {
-        return sizeY;
+        coloumns = newcoloumns;
+    }
+
+    int getRows() const
+    {
+        return rows;
+    }
+    int getColoumns() const
+    {
+        return coloumns;
     }
 
 private:
 
-    int sizeX; // Maze size X
-    int sizeY; // Maze size Y
+    int rows; // Maze size X
+    int coloumns; // Maze size Y
 
 };
+
+class Drawer
+{
+public:
+
+
+void addColor(uint32_t id, sf::Color color)
+{
+    colors.insert({id, color});
+}
+
+void drawMaze(sf::RenderWindow& window, const sf::IntRect& field, const Maze& maze)
+{
+    int tileHeight, tileWidth;
+
+    tileHeight = field.height / maze.getRows();
+    tileWidth = field.width / maze.getColoumns();
+
+    for(size_t i = 0; i < maze.tiles.size(); i++)
+    {
+        for(size_t j = 0; j < maze.tiles[i].size(); j++)
+        {
+            rectangleShape.setPosition(sf::Vector2f(field.getPosition().x + i * tileWidth, field.getPosition().y + j * tileHeight));
+            rectangleShape.setSize(sf::Vector2f(tileWidth, tileHeight));
+            rectangleShape.setFillColor(colors[maze.tiles[i][j].status]);
+            window.draw(rectangleShape);
+        }
+    }
+}
+
+private:
+
+    sf::RectangleShape rectangleShape;
+    std::unordered_map<uint32_t, sf::Color> colors;
+
+};
+
+int main(){
+
+    const int screenHeight = 750, screenWidth = 1500;
+
+	sf::RenderWindow app(sf::VideoMode(screenWidth, screenHeight), "", sf::Style::Default);
+	app.setFramerateLimit(60);
+
+    Maze a;
+    a.tiles[1][1].isWall;
+
+    for(int x = 0; x < a.getRows(); x++)
+    {
+        for(int y = 0; y < a.getColoumns(); y++)
+        {
+            a.tiles[x][y].isWall = false;
+        }
+    }
+
+    sf::Event e;
+
+	while (app.isOpen())
+	{
+		while (app.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+			{
+				app.close();
+			}
+		}
+		app.clear(sf::Color(69, 6, 147));
+
+
+        app.display();
+
+
+    }
+    //  Maze_drawer() : public Drawable;
+}
